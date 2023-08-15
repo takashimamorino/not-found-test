9ffaae1ca589548962d6f53c2c60db5fde6066a2
takashimamorino <does1026@gmail.com>
2023-08-15 12:16:09 +0900

    chore: tmp

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -9,6 +9,15 @@ const Test: NextPage<TTest> = ({ foo }) => {
 };
 
 export const getStaticProps: GetStaticProps = async () => {
+	throw new Error("test");
+	// if (err) {
+	// 	return {
+	// 		// notFound: true,
+	// 		// // props: { foo: "bar" },
+	// 		revalidate: 60,
+	// 		props: {},
+	// 	};
+	// }
 	return { props: { foo: "foo" }, revalidate: 60 };
 };
 
